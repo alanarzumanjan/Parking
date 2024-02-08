4948df21058a04e5d5ac6040bf198bf4267d1881
martinsmb <145760580+martinsmb@users.noreply.github.com>
2024-02-08 14:19:20 +0000

    Admin

--- Admin.cpp
+++ Admin.cpp
@@ -13,9 +13,10 @@ public:
     void change_ticket_cost()
     {
         int ticketType;
+        int price;
 
-        cout << "Choose ticket type:\n";
-        cout << "Press 1 for hour ticket\n";
+        cout << "Enter your ticket price:\n";
+        cout << "Press 1 for 1 hour ticket:\n";
         cout << "Press 2 for 2 hour ticket\n";
         cout << "Press 3 for 1 day ticket\n";
         cin >> ticketType;
@@ -23,13 +24,34 @@ public:
         switch (ticketType)
         {
         case 1:
-            cout << "Price for 1 hour ticket: $3\n";
+            cout << "Enter price for 1 hour ticket (between 1 and 10 dollars): ";
+            cin >> price;
+            if (price >= 1 && price <= 10) {
+                ticket_cost = price;
+                cout << "Price for 1 hour ticket: $" << ticket_cost << endl;
+            } else {
+                cout << "Invalid price! Please enter a value between 1 and 10 dollars.\n";
+            }
             break;
         case 2:
-            cout << "Price for 2 hour ticket: $5\n";
+            cout << "Enter price for 2 hour ticket (between 5 and 15 dollars): ";
+            cin >> price;
+            if (price >= 5 && price <= 15) {
+                ticket_cost = price;
+                cout << "Price for 2 hour ticket: $" << ticket_cost << endl;
+            } else {
+                cout << "Invalid price! Please enter a value between 5 and 15 dollars.\n";
+            }
             break;
         case 3:
-            cout << "Price for 1 day ticket: $30\n";
+            cout << "Enter price for 1 day ticket (between 25 and 40 dollars): ";
+            cin >> price;
+            if (price >= 1 && price <= 10) {
+                ticket_cost = price;
+                cout << "Price for 1 day ticket: $" << ticket_cost << endl;
+            } else {
+                cout << "Invalid price! Please enter a value between 25 and 40 dollars.\n";
+            }
             break;
         default:
             cout << "Invalid choice!\n";
