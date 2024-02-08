7f26be7016e65b117f3c13b6f235da007e525e93
martinsmb <145760580+martinsmb@users.noreply.github.com>
2024-02-08 13:40:30 +0000

    Admin functions

---
+++ Admin.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Admin
+{
+public:
+    int ticket_cost;
+
+    Admin(int tc) : ticket_cost(tc) {}
+
+    void displayticket_cost() const
+    {
+        cout << "Price: " << ticket_cost << endl;
+    }
+
+    void change_ticket_cost()
+    {
+        int ticketType;
+
+        cout << "Choose ticket type:\n";
+        cout << "Press 1 for hour ticket\n";
+        cout << "Press 2 for 2 hour ticket\n";
+        cout << "Press 3 for 1 day ticket\n";
+        cin >> ticketType;
+
+        switch (ticketType)
+        {
+        case 1:
+            cout << "Price for 1 hour ticket: $3\n";
+            break;
+        case 2:
+            cout << "Price for 2 hour ticket: $5\n";
+            break;
+        case 3:
+            cout << "Price for 1 day ticket: $30\n";
+            break;
+        default:
+            cout << "Invalid choice!\n";
+            break;
+        }
+    }
+};
+
+int main()
+{
+    Admin admin(0);
+
+    admin.change_ticket_cost();
+
+    return 0;
+}
