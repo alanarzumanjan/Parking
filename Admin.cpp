b392c31ff41cccd132f79b7723949321792e2699
martinsmb <145760580+martinsmb@users.noreply.github.com>
2024-02-09 08:54:50 +0000

    mama

--- Admin.cpp
+++ Admin.cpp
@@ -1,5 +1,112 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib> 
+#include <ctime>   
+
+using namespace std;
+
+class Admin {
+public:
+    static const int max_lots = 50;  
+    int ticket_cost;
+    vector<int> occupied_lots; // Moved to class scope
+    vector<int> free_lots; // Moved to class scope
+
+    Admin(int tc) : ticket_cost(tc) {}
+
+    void change_ticket_cost() {
+        int ticketType;
+        int price;
+
+        cout << "Enter your ticket price:\n";
+        cout << "Press 1 to set price for Default ticket\n";
+        cout << "Press 2 to set price for Vip ticket\n";
+        cin >> ticketType;
+
+        switch (ticketType) {
+            case 1:
+                cout << "Enter price for Default ticket (between 1 and 100 dollars): ";
+                cin >> price;
+                if (price >= 1 && price <= 100) {
+                    ticket_cost = price;
+                    cout << "Price for Default ticket: $" << ticket_cost << endl;
+                } else {
+                    cout << "Invalid price! Please enter a value between 1 and 100 dollars.\n";
+                }
+                break;
+            case 2:
+                cout << "Enter price for Vip ticket (between 5 and 100 dollars): ";
+                cin >> price;
+                if (price >= 5 && price <= 100) {
+                    ticket_cost = price;
+                    cout << "Price for Vip ticket: $" << ticket_cost << endl;
+                } else {
+                    cout << "Invalid price! Please enter a value between 5 and 100 dollars.\n";
+                }
+                break;
+            default:
+                cout << "Invalid choice!\n";
+                break;
+        }
+    }
+
+    void printout_of_free_and_occupied_lots() {
+        for (int i = 1; i <= max_lots; ++i) {
+            if (rand() % 2 == 0) { 
+                free_lots.push_back(i);
+            } else {
+                occupied_lots.push_back(i);
+            }
+        }
+
+        int choice;
+        cout << "Press 3 to see a Free Lot.\n";
+        cout << "Press 4 to see an Occupied Lot.\n";
+        cin >> choice;
+
+        switch(choice) {
+            case 3:
+                if (free_lots.empty()) {
+                    cout << "No free lots available.\n";
+                } else {
+                    int random = rand() % free_lots.size();
+                    cout << "Free Lot: " << free_lots[random] << endl;
+                }
+                break;
+
+            case 4:
+                if (occupied_lots.empty()) {
+                    cout << "No occupied lots available.\n";
+                } else {
+                    int random = rand() % occupied_lots.size();
+                    cout << "Occupied Lot: " << occupied_lots[random] << endl;
+                }
+                break;
+
+            default:
+                cout << "Invalid choice!\n";
+                break;
+        }
+    }
+};
+
+int main() {
+    srand(time(0));
+    Admin admin(0);
+
+    admin.change_ticket_cost();
+    admin.printout_of_free_and_occupied_lots();
+
+    return 0;
+}
+
+
+
+
+
+
+/*#include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -48,6 +155,18 @@ public:
             break;
         }
     }
+
+    void printout_of_free_and_occupied_Lots() {
+    cout << "Occupied Lots: ";
+    for(int i = 1; i <= max_lots; ++i) {
+        if(find(notfilled_lots.begin(), notfilled_lots.end(), i) != notfilled_lots.end()) {
+            cout << "Free ";
+        } else {
+            cout << "Occupied ";
+        }
+    }
+    cout << endl;
+}
 };
 
 int main()
@@ -57,4 +176,4 @@ int main()
     admin.change_ticket_cost();
 
     return 0;
-}
+}*/
