28a65b224f8c2508f0a00baa244b9d18cff958a4
mark <markkorobkin85@gmail.com>
2024-02-09 08:47:31 +0000

    mama

--- ticket.cpp
+++ ticket.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <vector>
+#include "park.cpp"
 
 using namespace std;
 
 class Ticket {
     private:
         vector<Ticket> tickets;
-    public:
+        static int next_ticket_id;
+    protected:
         int ticket_id;
+    public:
         int cost_hour = 20;
+        Parking parking;
         int place_number;
         float total_cost = 10;
-        Ticket(int id, int pn) : 
-            ticket_id(id), place_number(pn){}
+        Ticket() : ticket_id(next_ticket_id++), place_number(parking.notfilled_lots[ticket_id - 1]) {}
         
-        void printVipTicket() {
+        void printTicket() {
             // every next ticket id += 1
             // place_number должен выбираться из свободных мест
             // + спрос какой билет вы хотите
@@ -26,16 +29,34 @@ class Ticket {
 };
 
 class VipTicket: public Ticket {
-    int cost_hour = 40;
-    float total_cost = 15;
-    VipTicket(int ip, int pn) : Ticket(ip, pn){}
+    private:
+        vector<Ticket> viptickets;
+        static int next_vip_ticket_id;
+    protected:
+        int vip_ticket_id;
+    public:
+        Parking parking;
+        int place_number;
+        int cost_hour = 40;
+        float total_cost = 15;
+        VipTicket() : vip_ticket_id(next_vip_ticket_id++), place_number(parking.notfilled_lots[ticket_id - 1]){};
 
-    void printVipTicket() {
-        // every next ticket id += 1
-        // place_number должен выбираться из свободных мест
-        cout << "VIP ticket ID: " << ticket_id <<
-        "\nPlace number: " << place_number <<
-        "\nCost per hour: " << cost_hour <<
-        "\nTotal cost: " << total_cost << endl;
-    }
+        void printVipTicket() {
+            // every next ticket id += 1
+            // place_number должен выбираться из свободных мест
+            cout << "VIP ticket ID: " << ticket_id <<
+            "\nPlace number: " << place_number <<
+            "\nCost per hour: " << cost_hour <<
+            "\nTotal cost: " << total_cost << endl;
+        }
 };
+
+int Ticket::next_ticket_id = 1;
+int VipTicket::next_vip_ticket_id = 1;
+
+int main() {
+    Ticket* ticket1 = new Ticket();
+    VipTicket* ticket2 = new VipTicket();
+    ticket1->printTicket();
+    ticket2->printVipTicket();
+}
